695faa8369720a4a1435a3cd651af6bf11817bed
marcelochaves95 <marcelochaves17@gmail.com>
2022-06-19 20:39:10 -0300

    Create stack.h

---
+++ src/stack.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <iostream>
+#include "list.h"
+
+template<class T>
+class Stack
+{
+private:
+    List<T> list;
+public:
+    Stack()
+    {
+    }
+
+    ~Stack()
+    {
+        clear();
+    }
+
+    void push(const T& item)
+    {
+        list.add(item);
+    }
+
+    T& pop()
+    {
+        if (is_empty())
+        {
+            std::cout << "The stack is empty." << std::endl;
+            return;
+        }
+
+        T item = list.last();
+        list.remove_last();
+        return item;
+    }
+
+    T& peek()
+    {
+        return list.last();
+    }
+
+    bool contains(const T& item)
+    {
+        return list.contains(item);
+    }
+
+    bool is_empty() const
+    {
+        return list.is_empty();
+    }
+
+    int count() const
+    {
+        list.count();
+    }
+
+    void clear()
+    {
+        list.clear();
+    }
+};
